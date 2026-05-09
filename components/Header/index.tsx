75110d8c51c70fd4bd695016cc1e2fa552ddf190
todatoday <taki.souad-90@hotmail.com>
2026-05-09 10:03:30 +0200

    header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div>
+      {/* Logo
+           Navigation Links
+           Toggle button(mobile menu) */}
+
+      <main>
+        <h1>My recipe app</h1>
+      </main>
+    </div>
+  );
+};
+export default Header;
